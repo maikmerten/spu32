1f802fdedcf6636b79b5a972dec6537183f75567
Maik Merten <maikmerten@googlemail.com>
2018-05-17 22:08:29 +0200

    serial bootloader: working code for flashing SPI flash via UART

--- software/spu32-serial-bootloader/src/main/java/de/maikmerten/spu32/serialbootloader/Main.java
+++ software/spu32-serial-bootloader/src/main/java/de/maikmerten/spu32/serialbootloader/Main.java
@@ -1,8 +1,10 @@
 package de.maikmerten.spu32.serialbootloader;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Random;
 
 /**
  *
@@ -25,18 +27,11 @@ public class Main {
         SerialConnection conn = new SerialConnection(uartDevice, 115200);
         BootloaderProtocol bp = new BootloaderProtocol(conn);
         
-        SPIFlasher flasher = new SPIFlasher(bp);
-        flasher.enableWrite();
-        flasher.eraseChip();
-        while(flasher.isBusy()) {
-            System.out.print(".");
-        }
-        System.out.println();
-        
+      
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         File f = new File(programFile);
         FileInputStream fis = new FileInputStream(f);
-        byte[] buf = new byte[512];
+        byte[] buf = new byte[64];
         int read = fis.read(buf);
         while(read > 0) {
             baos.write(buf, 0, read);
@@ -45,6 +40,44 @@ public class Main {
         fis.close();
         
         byte[] uploadData = baos.toByteArray();
+        
+        byte[] randomData = new byte[512 * 1024];
+        Random r = new Random();
+        r.nextBytes(randomData);
+        
+        ByteArrayInputStream bais = new ByteArrayInputStream(randomData);
+        SPIFlasher flasher = new SPIFlasher(bp);
+        flasher.enableWrite();
+        System.out.print("erasing chip ");
+        flasher.eraseChip();
+        while(flasher.isBusy()) {
+            System.out.print(".");
+        }
+        
+  
+        System.out.println();
+        read = bais.read(buf);
+        int chunk = 0;
+        while(read > 0) {
+            System.out.println("programming 64-byte chunk " + chunk + " ");
+            flasher.enableWrite();
+            flasher.programChunk(chunk, buf);
+            while(flasher.isBusy()) {
+                System.out.print(".");
+            }
+            byte[] chunkData = flasher.readChunk(chunk);
+            for(int i = 0; i < buf.length; ++i) {
+                if(chunkData[i] != buf[i]) {
+                    throw new Exception("read chunk data does not match!");
+                }
+            }
+            
+            System.out.println();
+            chunk++;
+            read = bais.read(buf);
+        }
+        
+        
         
         System.out.println("Uploading " + uploadData.length + " bytes...");
         bp.uploadWithUART(0, uploadData);
--- software/spu32-serial-bootloader/src/main/java/de/maikmerten/spu32/serialbootloader/SPIFlasher.java
+++ software/spu32-serial-bootloader/src/main/java/de/maikmerten/spu32/serialbootloader/SPIFlasher.java
@@ -1,5 +1,7 @@
 package de.maikmerten.spu32.serialbootloader;
 
+import java.io.ByteArrayOutputStream;
+
 /**
  *
  * @author maik
@@ -7,6 +9,7 @@ package de.maikmerten.spu32.serialbootloader;
 public class SPIFlasher {
     
     private final byte CMD_CHIP_ERASE = (byte) 0x60;
+    private final byte CMD_FASTREAD = (byte) 0x0B;
     private final byte CMD_PAGE_PROGRAM = (byte) 0x02;
     private final byte CMD_READ_STATUS = (byte) 0x05;
     private final byte CMD_WRITE_ENABLE = (byte) 0x06;
@@ -27,14 +30,57 @@ public class SPIFlasher {
         byte[] op = {CMD_READ_STATUS, CMD_READ_STATUS};
         byte[] response = bp.writeToSPI(op);
         
-        System.out.println("busy response: " + response[1]);
-        
         return (response[1] & 0x01) != 0;
     }
     
     public void enableWrite() throws Exception {
         byte[] op = {CMD_WRITE_ENABLE};
         bp.writeToSPI(op);
+    }
+    
+    private byte[] addr24(int addr) {
+        byte b0 = (byte)((addr >> 16) & 0xFF);
+        byte b1 = (byte)((addr >> 8) & 0xFF);
+        byte b2 = (byte)(addr & 0xFF);
+        byte[] result = {b0, b1, b2};
+        return result;
+    }
+    
+    public void programChunk(int chunknum, byte[] data) throws Exception {
+        if(data.length != 64) {
+            throw new IllegalArgumentException("chunk programming requires 64 bytes of data");
+        }
+        byte[] op = {CMD_PAGE_PROGRAM};
+        byte[] addr = addr24(chunknum << 6);
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(op);
+        baos.write(addr);
+        baos.write(data);
+        bp.writeToSPI(baos.toByteArray());
+        
+    }
+    
+    public byte[] readChunk(int chunknum) throws Exception {
+        byte[] result = new byte[64];
+        
+        byte[] op = {CMD_FASTREAD};
+        byte[] addr = addr24(chunknum << 6);
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(op);
+        baos.write(addr);
+        baos.write(op); // dummy byte
+        baos.write(result); // 64 dummy bytes
+        
+        byte[] received = bp.writeToSPI(baos.toByteArray());
+        
+        int idx = 0;
+        for(int i = 5; i < received.length; ++i) {
+            result[idx++] = received[i];
+        }
+        
+        return result;
     }
     
 }
