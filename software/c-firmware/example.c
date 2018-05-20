c77291274db4d56b012103416c0f2cf86f45e910
Maik Merten <maikmerten@googlemail.com>
2018-05-20 18:24:16 +0200

    expand C-example a tiny bit

--- software/c-firmware/example.c
+++ software/c-firmware/example.c
@@ -3,14 +3,15 @@
 int main() {
 
 	char name[16];
-	printf("Your name: ");
-	fgets(name, sizeof name, 0);
-	printf("%s", name);
 
 	int i = 0;
 	while(1) {
+		printf("Your name: ");
+		fgets(name, sizeof name, 0);
+		printf("\r\n");
+
 		printf("Hi %s!\r\n", name);
-		printf("This is message %d\r\n", i);
+		printf("This is message %d\r\n\r\n", i);
 
 		i++;
 	}
