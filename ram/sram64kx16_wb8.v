333bb7f7841b1002e0566a8df128790e6af52f3d
Maik Merten <maikmerten@googlemail.com>
2018-06-25 16:42:36 +0200

    slightly more stable SRAM access (still plenty unstable)

--- ram/sram64kx16_wb8.v
+++ ram/sram64kx16_wb8.v
@@ -15,28 +15,39 @@ module sram64kx16_wb8
 		output O_ce, O_oe, O_we, O_ub, O_lb
 	);
 
+	reg[16:0] adr_buf;
+	reg[7:0] dat_buf;
 	
 	wire write = STB_I & WE_I;
 	wire read = STB_I & !WE_I;
-	wire upper_byte = ADR_I[0];
+	wire upper_byte = adr_buf[0];//ADR_I[0];
 	wire lower_byte = !upper_byte;
 
 	// tristate data line to SRAM
-	assign IO_data = write ? {DAT_I, DAT_I} : 16'bz;
+	assign IO_data = write ? {dat_buf, dat_buf} : 16'bz;
 	// select requested 8 bits from 16 bit data line
-	assign DAT_O = lower_byte ? IO_data[15:8] : IO_data[7:0];
+	//assign DAT_O = lower_byte ? IO_data[15:8] : IO_data[7:0];
 
-	assign O_address = ADR_I[16:1];
+	assign O_address = adr_buf[16:1]; //ADR_I[16:1];
 
 	// control signals are active low, thus negated
-	assign O_ce = !(write | read);
+	assign O_ce = 0; //!(write | read);
 	assign O_oe = !read;
 	assign O_we = !write;
 	assign O_lb = !lower_byte;
 	assign O_ub = !upper_byte;
 
 	always @(posedge CLK_I) begin
+		if(STB_I) begin
+			adr_buf <= ADR_I;
+			dat_buf <= DAT_I;
+		end
+
 		ACK_O <= STB_I;
+	end
+
+	always @(negedge CLK_I) begin
+		DAT_O <= lower_byte ? IO_data[15:8] : IO_data[7:0];
 	end
 
 endmodule
