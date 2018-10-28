6f7ce00a39c13c59912ce1546d4f9dd39839cff6
Maik Merten <maikmerten@googlemail.com>
2018-10-28 15:47:07 +0100

    Dhrystone!

---
+++ software/c-firmware/dhrystone/crt0.s
@@ -0,0 +1,28 @@
+
+.section .text
+ 
+.global _start
+_start:
+	# reset vector at 0x0
+	.=0x0
+	j _init
+
+# interrupt handler
+.=0x10
+_interrupt:
+	# for now just do an endless loop
+	j _interrupt
+
+
+
+_init:
+	# set up stack pointer
+	li sp,(512 * 1024)
+
+	# call main function
+	jal ra,main
+
+	# back to start
+#	j _start
+_loop:
+	j _loop
