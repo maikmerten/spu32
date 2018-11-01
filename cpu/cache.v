bbfe90a4055cec30fa14a2d6e313d8900c705138
Maik Merten <maikmerten@googlemail.com>
2018-11-01 13:18:02 +0100

    make cache size easily adjustable

--- cpu/cache.v
+++ cpu/cache.v
@@ -13,34 +13,42 @@ module cache(
 		output O_hit
 	);
 
-	reg[15:0] tags[255:0]; // 256 tags, each 16 bit: 1 bit for "valid", 15 bits for address
-	reg[31:0] data[255:0]; // 256 words of storage
+	// ENTRYBITS determines the size of the cache, everything else is computed from this
+	localparam ENTRYBITS = 10;
+	localparam ENTRIES = 2**ENTRYBITS;
+	localparam ENTRYLOBIT = 2;
+	localparam ENTRYHIBIT = ENTRYBITS + 1;
+	localparam TAGLOBIT = ENTRYHIBIT + 1;
+	localparam TAGHIBIT = ENTRYHIBIT + 15;
+
+	reg[15:0] tags[ENTRIES-1:0]; // 256 tags, each 16 bit: 1 bit for "valid", 15 bits for address
+	reg[31:0] data[ENTRIES-1:0]; // 256 words of storage
 
 	wire cacheable_addr, cacheable_read, update_cache;
-	wire[7:0] entry;
+	wire[ENTRYBITS-1:0] entry;
 	wire[14:0] tagaddr;
 
 	// only aligned words are cached
-	assign cacheable_addr = (I_addr[31:25] == 0 && I_addr[1:0] == 0);
+	assign cacheable_addr = (I_addr[31:TAGHIBIT+1] == 0 && I_addr[1:0] == 0);
 	assign cacheable_read = cacheable_addr && I_busop == `BUSOP_READW;
 	assign update_cache = cacheable_read && I_offer_data;
-	assign entry = I_addr[9:2];
-	assign tagaddr = I_addr[24:10];
+	assign entry = I_addr[ENTRYHIBIT:ENTRYLOBIT];
+	assign tagaddr = I_addr[TAGHIBIT:TAGLOBIT];
 
 	// react to writes that can invalidate a cache line
 	wire invalidate;
-	wire[7:0] entry_invalidate;
-	assign invalidate = I_invalidate_addr[31:25] == 0 && (I_busop == `BUSOP_WRITEB || I_busop == `BUSOP_WRITEH || I_busop == `BUSOP_WRITEW);
-	assign entry_invalidate = I_invalidate_addr[9:2];
+	wire[ENTRYBITS-1:0] entry_invalidate;
+	assign invalidate = I_invalidate_addr[31:TAGHIBIT+1] == 0 && (I_busop == `BUSOP_WRITEB || I_busop == `BUSOP_WRITEH || I_busop == `BUSOP_WRITEW);
+	assign entry_invalidate = I_invalidate_addr[ENTRYHIBIT:ENTRYLOBIT];
 
 	// tag content
 	reg[15:0] tag;
 	assign O_hit = cacheable_read && tag == {1'b1, tagaddr};
 
-	reg[7:0] entrycounter = 0;
+	reg[ENTRYBITS-1:0] entrycounter = 0;
 
 	wire[15:0] tagdata;
-	wire[7:0] tagentry;
+	wire[ENTRYBITS-1:0] tagentry;
 	wire writetag;
 	// on reset or invalidate, tags are zeroed out to get rid of the 'valid' flag
 	assign tagdata = (I_reset || invalidate) ? 16'h0000 : {1'b1, tagaddr};
@@ -50,7 +58,7 @@ module cache(
 	// write to tag on reset, invalidate or when something is to be cached
 	assign writetag = I_reset || invalidate || (I_en && update_cache);
 
-	// TODO: invalidate on write
+
 	always @(posedge I_clk) begin
 
 		if(I_en) begin
