a4d388dde01bdb1a31665cecded60f320528e71d
Maik Merten <maikmerten@googlemail.com>
2019-04-08 16:39:42 +0200

    stability fix for write accesses to VGA-enabled SRAM

--- ram/sram512kx8_wb8_vga.v
+++ ram/sram512kx8_wb8_vga.v
@@ -38,6 +38,7 @@ module sram512kx8_wb8_vga
 
 	always @(posedge CLK_I) begin
 		O_oe <= 1; // active low
+		O_data <= DAT_I;
 		O_output_enable <= 0;
 		we_buf <= WE_I;
 		stb_buf <= STB_I;
@@ -49,7 +50,6 @@ module sram512kx8_wb8_vga
 			O_oe <= 0;
 		end else if(STB_I) begin
 			O_address <= ADR_I;
-			O_data <= DAT_I;
 			O_oe <= !(!WE_I); // active low
 			O_output_enable <= WE_I;
 		end
