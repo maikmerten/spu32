7640375d5e5a71a6479bf23e0c91ef1a06cf5310
Maik Merten <maikmerten@googlemail.com>
2019-05-02 18:47:54 +0200

    decoder: Keep complete instruction word to keep decoder output stable regardless of bus operation (with exception of rs1 and rs2)

--- cpu/decoder.v
+++ cpu/decoder.v
@@ -9,24 +9,31 @@ module decoder(
 	output[4:0]	O_rs1, O_rs2,
 	output reg[4:0] O_rd,
 	output reg[31:0] O_imm,
-	output reg[4:0] O_opcode,
+	output[4:0] O_opcode,
 	output[2:0] O_funct3,
 	output reg[6:0] O_funct7,
 	output reg[5:0] O_branchmask
 	);
 
+	reg[31:0] instr;
+
 	reg[4:0] opcode;
 	reg[31:0] imm;
 	reg[2:0] funct3;
 	assign O_funct3 = funct3;
+	assign O_opcode = opcode;
 
 	// combinatorial decode of source register information to allow for register read during decode
 	assign O_rs1 = I_instr[19:15];
 	assign O_rs2 = I_instr[24:20];
 
+	reg isbranch = 0;
 
 	always @(*) begin
-		opcode = I_instr[6:2];
+		opcode = instr[6:2];
+		O_funct7 = instr[31:25];
+		O_rd = instr[11:7];
+		funct3 = instr[14:12];
 
 		case(opcode)
 			`OP_STORE: imm = {{20{I_instr[31]}}, I_instr[31:25], I_instr[11:8], I_instr[7]}; // S-type
@@ -36,25 +43,24 @@ module decoder(
 			default: imm = {{20{I_instr[31]}}, I_instr[31:20]}; // I-type and R-type. Immediate has no meaning for R-type instructions
 		endcase
 
+		O_imm = imm;
+
+		isbranch = (opcode == `OP_BRANCH);
 		O_branchmask = 0;
-		case(funct3) // funct3
-            `FUNC_BEQ:  O_branchmask[0] = 1;
-            `FUNC_BNE:  O_branchmask[1] = 1;
-            `FUNC_BLT:  O_branchmask[2] = 1;
-            `FUNC_BGE:  O_branchmask[3] = 1;
-            `FUNC_BLTU: O_branchmask[4] = 1;
-            `FUNC_BGEU: O_branchmask[5] = 1;
+		case(funct3)
+            `FUNC_BEQ:  O_branchmask[0] = isbranch;
+            `FUNC_BNE:  O_branchmask[1] = isbranch;
+            `FUNC_BLT:  O_branchmask[2] = isbranch;
+            `FUNC_BGE:  O_branchmask[3] = isbranch;
+            `FUNC_BLTU: O_branchmask[4] = isbranch;
+            `FUNC_BGEU: O_branchmask[5] = isbranch;
 		endcase
 
 	end
 
 	always @(posedge I_clk) begin
 		if(I_en) begin
-			O_opcode <= opcode;
-			funct3 <= I_instr[14:12];
-			O_funct7 <= I_instr[31:25];
-			O_rd <= I_instr[11:7];
-			O_imm <= imm;
+			instr <= I_instr;
 		end
 	end
 
