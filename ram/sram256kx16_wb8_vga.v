3f7bd8a032ceecef81d0bc4b1067dd0d3f0dbc33
Maik Merten <maikmerten@googlemail.com>
2019-11-22 13:59:15 +0100

    sram256kx16: Only output data from FPGA to SRAM during write pulse, be a bit more tidy here and there

--- ram/sram256kx16_wb8_vga.v
+++ ram/sram256kx16_wb8_vga.v
@@ -16,7 +16,7 @@ module sram256kx16_wb8_vga
 
 		// SRAM signals
 		input[15:0] I_data,
-		output reg [15:0] O_data,
+		output[15:0] O_data,
 		output[17:0] O_address,
 		output O_oe, O_ce, O_we, O_lb, O_ub,
 
@@ -27,9 +27,14 @@ module sram256kx16_wb8_vga
 	
 	reg write1 = 0;
 	reg write2 = 0;
+	wire writepulse = (write1 != write2);
 
-	reg output1 = 0;
-	reg output2 = 0;
+	reg read1 = 0;
+	reg read2 = 0;
+	wire readpulse = (read1 != read2);
+
+	reg[7:0] writedata;
+	assign O_data = {writedata, writedata};
 
 	reg[18:0] address;
 	assign O_address = address[18:1];
@@ -38,27 +43,25 @@ module sram256kx16_wb8_vga
 
 	// control signals are active low, thus negated
 	assign O_ce = 0;
-	assign O_we = !(write1 != write2);
-	assign O_oe = !(output1 != output2);
+	assign O_we = !writepulse;
+	assign O_oe = !readpulse;
 
+	assign O_output_enable = writepulse;
 	assign O_wb_stall = I_wb_stb & I_vga_req;
 
 	always @(posedge I_wb_clk) begin
-		O_data <= {I_wb_dat, I_wb_dat};
-		O_output_enable <= 0;
+		writedata <= I_wb_dat;
 	
 		if(I_vga_req) begin
 			address <= I_vga_adr;
-			O_output_enable <= 0;
-			output1 <= !output2;
+			read1 <= !read2; // initiate read
 		end else if(I_wb_stb) begin
 			address <= I_wb_adr;
 			if(I_wb_we) begin
-				write1 <= !write2;
+				write1 <= !write2; // initiate write
 			end else begin
-				output1 <= !output2;
+				read1 <= !read2; // initiate read
 			end
-			O_output_enable <= I_wb_we;
 		end
 
 		O_wb_ack <= (I_wb_stb & !I_vga_req);
@@ -67,7 +70,7 @@ module sram256kx16_wb8_vga
 	always @(negedge I_wb_clk) begin
 		O_wb_dat <= address[0] ? I_data[15:8] : I_data[7:0];
 		write2 <= write1;
-		output2 <= output1;
+		read2 <= read1;
 	end
 
 endmodule
