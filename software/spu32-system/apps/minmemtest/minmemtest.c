4d220e9d4d37a7d083af81441a404f602032cc5f
Maik Merten <maikmerten@googlemail.com>
2021-02-09 18:04:21 +0100

    expand minmemtest (uint16 and uint32 read/writes)

--- software/spu32-system/apps/minmemtest/minmemtest.c
+++ software/spu32-system/apps/minmemtest/minmemtest.c
@@ -33,19 +33,25 @@ int main()
 	// when testing the SRAM controller, SRAM will begin at 0x01000000
 	uint32_t offset = 0x01000000;
 
-	// without SRAM, theres 8 KB of BRAM, looping over and over
-	uint32_t memsize = 8 * 1024;
+	// test a good chunk of memory
+	uint32_t memsize = 32 * 1024;
 
 	while (1)
 	{
-		const uint32_t base = offset + 256; // don't start at zero to avoid overwriting this program
+		const uint32_t base = offset + 1024; // don't start at zero to avoid overwriting this program
 		const uint32_t end = offset + memsize;
 
 		volatile uint8_t *bytePtr;
+		volatile uint16_t *u16Ptr;
+		volatile uint32_t *u32Ptr;
 
 		uint32_t pass = 0;
 
 		while(1) {
+
+			///////////////////////////
+			// byte tests
+			///////////////////////////			
 
 			set_prng_seed(pass);
 			// write predictable random numbers to memory
@@ -79,6 +85,135 @@ int main()
 					};
 				}
 			}
+
+			///////////////////////////
+			// half-word tests
+			///////////////////////////
+
+			set_prng_seed(pass);
+			// write predictable random numbers to memory
+			for(u16Ptr = (volatile uint16_t*)base;(uint32_t)u16Ptr < end; u16Ptr++) {
+				uint16_t prn = (uint16_t)(get_prng_value() & 0xFFFF);
+				*u16Ptr = prn;
+			}
+
+			// reset predictable number generator
+			set_prng_seed(pass);
+			// read memory contents and compare
+			for(u16Ptr = (volatile uint16_t*)base;(uint32_t)u16Ptr < end; u16Ptr++) {
+				uint16_t prn = (uint16_t)(get_prng_value() & 0xFFFF);
+				uint16_t val = *u16Ptr;
+				if(prn != val) {
+					while(1){
+						uint32_t now = get_time();
+
+						
+						set_leds(0x1);
+						while(get_time() < now + 800){};
+						
+						// clear LEDS, long pause
+						set_leds(0);
+						while(get_time() < now + 1600){};
+					};
+				}
+			}
+
+			///////////////////////////
+			// unaligned half-word tests
+			///////////////////////////
+
+			set_prng_seed(pass);
+			// write predictable random numbers to memory
+			for(u16Ptr = (volatile uint16_t*)base + 1;(uint32_t)u16Ptr < end; u16Ptr++) {
+				uint16_t prn = (uint16_t)(get_prng_value() & 0xFFFF);
+				*u16Ptr = prn;
+			}
+
+			// reset predictable number generator
+			set_prng_seed(pass);
+			// read memory contents and compare
+			for(u16Ptr = (volatile uint16_t*)base + 1;(uint32_t)u16Ptr < end; u16Ptr++) {
+				uint16_t prn = (uint16_t)(get_prng_value() & 0xFFFF);
+				uint16_t val = *u16Ptr;
+				if(prn != val) {
+					while(1){
+						uint32_t now = get_time();
+
+						
+						set_leds(0x2);
+						while(get_time() < now + 800){};
+						
+						// clear LEDS, long pause
+						set_leds(0);
+						while(get_time() < now + 1600){};
+					}
+				}
+			}
+
+			///////////////////////////
+			// word tests
+			///////////////////////////
+
+			set_prng_seed(pass);
+			// write predictable random numbers to memory
+			for(u32Ptr = (volatile uint32_t*)base;(uint32_t)u32Ptr < end; u32Ptr++) {
+				uint32_t prn = (uint32_t)get_prng_value();
+				*u32Ptr = prn;
+			}
+
+			// reset predictable number generator
+			set_prng_seed(pass);
+			// read memory contents and compare
+			for(u32Ptr = (volatile uint32_t*)base;(uint32_t)u32Ptr < end; u32Ptr++) {
+				uint32_t prn = (uint32_t)get_prng_value();
+				uint32_t val = *u32Ptr;
+				if(prn != val) {
+					while(1){
+						uint32_t now = get_time();
+
+						
+						set_leds(0x3);
+						while(get_time() < now + 800){};
+						
+						// clear LEDS, long pause
+						set_leds(0);
+						while(get_time() < now + 1600){};
+					}
+				}
+			}
+
+			///////////////////////////
+			// unaligend word tests
+			///////////////////////////
+
+			set_prng_seed(pass);
+			// write predictable random numbers to memory
+			for(u32Ptr = (volatile uint32_t*)base+1;(uint32_t)u32Ptr < end; u32Ptr++) {
+				uint32_t prn = (uint32_t)get_prng_value();
+				*u32Ptr = prn;
+			}
+
+			// reset predictable number generator
+			set_prng_seed(pass);
+			// read memory contents and compare
+			for(u32Ptr = (volatile uint32_t*)base+1;(uint32_t)u32Ptr < end; u32Ptr++) {
+				uint32_t prn = (uint32_t)get_prng_value();
+				uint32_t val = *u32Ptr;
+				if(prn != val) {
+					while(1){
+						uint32_t now = get_time();
+
+						
+						set_leds(0x4);
+						while(get_time() < now + 800){};
+						
+						// clear LEDS, long pause
+						set_leds(0);
+						while(get_time() < now + 1600){};
+					}
+				}
+			}
+
 
 			set_leds(pass & 0xFF);
 
