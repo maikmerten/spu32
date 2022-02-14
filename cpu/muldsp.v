525dfea9f9bb72c41ecaa01d5ae7ca266bb7aeb1
Maik Merten <maikmerten@googlemail.com>
2022-02-14 09:25:11 +0100

    muldsp: use single 33x33->64 bit signed multiplication with zero/sign-extension of operands

--- cpu/muldsp.v
+++ cpu/muldsp.v
@@ -17,67 +17,25 @@ module spu32_cpu_muldsp(
     assign O_busy = 0;
 
 
-    // determine signedness according to MUL-operation
-    reg s1_signed, s2_signed;
+    // determine sign/zero-extension for operands
+    reg s1_extension, s2_extension;
     always @(*) begin
+        s1_extension = 1'b0;
+        s2_extension = 1'b0;
         case(I_op)
             `ALUOP_MULH: begin
-                s1_signed = 1'b1;
-                s2_signed = 1'b1;
+                s1_extension = I_s1[31]; // sign-extend s1
+                s2_extension = I_s2[31]; // sign-extend s2
             end
             `ALUOP_MULHSU: begin
-                s1_signed = 1'b1;
-                s2_signed = 1'b0;
-            end
-            default: begin
-                s1_signed = 1'b0;
-                s2_signed = 1'b0;
+                s1_extension = I_s1[31]; // sign-extend s1
             end
         endcase
     end
+ 
 
-/*
-    // Alternative approach with only one unsigned multiplier. This keeps track
-    // of the sign-bits and uses two-complement operations to ensure correct
-    // signedness. However, this adds plenty of logic for negating bits and
-    // and doing the +1 to convert two-complement representations.
-    assign O_busy = 0;
-    wire[63:0] result;
-
-    wire s1_negative = I_s1[31] & s1_signed;
-    wire s2_negative = I_s2[31] & s2_signed;
-    wire result_negative = s1_negative ^ s2_negative;
-
-    wire[31:0] s1 = s1_negative ? ~I_s1 + 1 : I_s1;
-    wire[31:0] s2 = s2_negative ? ~I_s2 + 1 : I_s2;
-
-    wire[63:0] mult = s1 * s2;
-
-    always @(*) begin
-        result = result_negative ? ~mult + 1 : mult;
-    end
-    assign O_result = result;*/
-    
-    // This approach does three multiplications in parallel (DSP-heavy!)
-    // and chooses the upmost 32-bit according to operation.
-
-    wire[63:0] mul_signed = $signed(I_s1) * $signed(I_s2);
-    wire[63:0] mul_unsigned = $unsigned(I_s1) * $unsigned(I_s2);
-    // In Verilog, mixed signed/unsigned multiplications are *unsigned*!
-    // Thus do signed multiplication with zero-extension for the unsigned
-    // operand.
-    wire[63:0] mul_signed_unsigned = $signed(I_s1) * $signed({1'b0,I_s2});
-
-    reg[31:0] mulhi;
-    always @(*) begin
-        case({s1_signed, s2_signed})
-            2'b00: mulhi[31:0] = mul_unsigned[63:32];
-            2'b11: mulhi[31:0] = mul_signed[63:32];
-            default: mulhi[31:0] = mul_signed_unsigned[63:32];
-        endcase
-    end
-    wire[63:0] result = {mulhi, mul_signed[31:0]};
-    assign O_result = result;
+    wire[63:0] mul_signed = $signed({s1_extension, I_s1}) * $signed({s2_extension, I_s2});
+    assign O_result = mul_signed;
 
 endmodule
 
